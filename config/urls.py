07f5ea2c0f6e5f0b9a9cf9cef4751a45d3a61e63
Jacob Powers <powersjcb@gmail.com>
2015-09-01 19:20:20 -0700

    Add routes and start playing with REST API tools

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
 
     # Your stuff: custom urls includes go here
     url(r'^flavors/', include("two_scoops_project.flavors.urls", namespace="flavors")),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
